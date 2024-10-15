45b6475f5c935fdf9f48d0d120523f82437d8179
Javiera Venegas <javieravenegas.arch@gmail.com>
2024-10-14 22:05:07 -0300

    navbar

--- frontend/src/assets/components/NavBar.jsx
+++ frontend/src/assets/components/NavBar.jsx
@@ -3,34 +3,40 @@ import { NavLink } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
 
 const NavBar = () => {
-    const setActiveClass = ({ isActive }) => (isActive ? "linkActivo" : "linkInactivo");
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
             <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">Planfiz</NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <NavLink className="navbar-brand p-1 logo" to="/">Planfiz</NavLink>
+                <button className="navbar-toggler" 
+                        type="button" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#navbarNav" 
+                        aria-controls="navbarNav" 
+                        aria-expanded="false" 
+                        aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <NavLink className={setActiveClass} aria-current="page" to="/">Home</NavLink>
+                            <NavLink className={'({ isActive }) => (isActive ? "active" : "") me-4'} aria-current="page" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink  className={setActiveClass} to="/register">Register</NavLink>
+                            <NavLink  className={'({ isActive }) => (isActive ? "active" : "") me-4'}to="/register">Register</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className={setActiveClass} to="/login">Login</NavLink>
+                            <NavLink className={'({ isActive }) => (isActive ? "active" : "") me-4'} to="/login">Login</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className={setActiveClass} to="/product">Products</NavLink>
+                            <NavLink className={'({ isActive }) => (isActive ? "active" : "") me-4'} to="/product">Products</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink  className={setActiveClass} to="/profile">Profile </NavLink>
+                            <NavLink  className={'({ isActive }) => (isActive ? "active" : "") me-4'} to="/profile">Profile </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink  className={setActiveClass} to="/shoppingCart"> <FiShoppingCart/> </NavLink>
+                            <NavLink  className={'({ isActive }) => (isActive ? "active" : "") me-4'} to="/shoppingCart"> <FiShoppingCart/> </NavLink>
                         </li>
                         
                     </ul>
